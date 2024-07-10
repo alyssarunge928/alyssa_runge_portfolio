0063340bee5cdfa943b5ec0615e11d3da5593f3a
alyssarunge928 <alyssarunge928@gmail.com>
2024-07-09 20:55:03 -0700

    updated class projects

--- src/Components/AboutMe/AboutMe.jsx
+++ src/Components/AboutMe/AboutMe.jsx
@@ -18,11 +18,17 @@ const AboutMe = () => {
         </div>
         <div>
           <p>
-            A storytellers job is to captivate the audience, using their words to get the readers attention, and keep them with the magic of entertainment.
-          </p>
+          
+          </p>A storyteller's job is to captivate the audience with eye-catching words and a compelling call to action that sparks interest and engagement.
+          Encouraged by a creative writing teacher who praised my talent from the first assignment, I have always had a knack for weaving words into captivating narratives. Friends who have visited psychics have described me as the empath in their lives, with an uncanny accuracy that speaks to my intuitive nature.
+          Born in Maui, I was immersed in the island's rich culture from a young age. Growing up in Arizona and working with hospice patients and their families, I evolved my empathy and compassion. These experiences have shaped my strong sense of self and commitment to compassionate service and emotional connection.
+          The pandemic prompted reflection and growth, leading me to pursue my passion for writing. Recognizing my strongest skill, I have been diligently working towards becoming a copywriter, ready to craft stories that captivate and inspire.
         </div>
+        
+  
       </div>
     </div>
+
   );
 };
 
--- src/Components/ThemeBannerText/ThemeBannerText.jsx
+++ src/Components/ThemeBannerText/ThemeBannerText.jsx
@@ -2,14 +2,13 @@ const ThemeBannerText = () => {
   return (
     <div className="min-h-screen text-center justify-center items-center flex font-primary-one -mb-52">
       <h1 className="text-center lg:text-5xl md:text-4xl text-2xl -mt-80 lg:-mt-96 w-4/6 mx-auto">
-        Hello {"I'm Alyssa "} <br /> <br />
+        Hello, {"I'm Alyssa"} <br /> <br />
+       
         <span className="">
-          Embrace uniqueness, overcome challenges.Enter here your 17 words
-          tagline/sentence
+          I'm a Phoenix-based Copywriter and Proofreading expert.
         </span>
       </h1>
     </div>
   );
 };
-
 export default ThemeBannerText;
